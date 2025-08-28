01dc24602101ae50147e7a8968943429c2613f54
LucaIapadre98 <luca.iap.1998@gmail.com>
2025-08-28 16:29:07 +0200

    snack 2

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
+//#SNACK 1
 // Hai ricevuto un dato generico da un'API, ma non sai di che tipo sia… 
 // Il tuo compito è controllare il tipo del dato e stampare il valore in modo corretto.
 // Se è una stringa: stampala in maiuscole
 // Se è un numero: moltiplicalo per due e stampalo
 // Se è un booleano: stampa “Sì” o “No” in base al suo valore
 // In tutti gli altri casi: stampa “Tipo non supportato”
+// Se è null: stampa “Il dato è vuoto”
+// Se è un array: stampa la sua lunghezza
+// Se è una Promise: attendi che si risolva e stampa il valore del resolve
 let dato : unknown;                                            // dato di tipo sconosciuto
 
 if(typeof dato === 'string'){                                  // controllo se è una stringa
@@ -23,6 +27,23 @@ if(typeof dato === 'string'){                                  // controllo se �
 };
 
 
-// Se è null: stampa “Il dato è vuoto”
-// Se è un array: stampa la sua lunghezza
-// Se è una Promise: attendi che si risolva e stampa il valore del resolve.
+
+
+//#SNACK 2
+// Crea un type alias Dipendente che rappresenta un lavoratore con i seguenti dati:
+// nome → stringa
+// cognome → stringa
+// annoNascita → numero
+// sesso → Può essere solo "m" o "f".
+// anniDiServizio (array di numeri, es. [2014, 2015, 2017, 2018])
+// Il type alias Dipendente, ha anche i seguenti dati:
+// emailAziendale → Email assegnata al dipendente (non si può modificare)
+// contratto → Specifica il tipo di contratto del dipendente, con valori limitati a “indeterminato”, “determinato” o “freelance”.
+
+type Dipendente = {
+  nome: string,
+  cognome: string,
+  annoNascita: number,
+  sesso: 'm' | 'f',
+  anniDiServizio: number[]
+};
